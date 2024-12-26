710bfebff2c86086ef70af4c2335ceaa946e1f58
russell <kangyunkui@egeyed.com>
2024-12-26 14:24:43 +0800

    Update log_service.go

--- works/admins/service/log_service.go
+++ works/admins/service/log_service.go
@@ -1,20 +1,15 @@
 package service
 
 import (
-	"admins/api/dto"
-	"context"
+	"admins/service/dto"
+
+	"github.com/gin-gonic/gin"
 )
 
 type LogService interface {
 	// 操作日志
-	CreateOperationLog(ctx context.Context, log *dto.OperationLogCreateRequest) error
-	ListOperationLogs(ctx context.Context, query dto.LogQueryRequest) (*dto.PageResponse, error)
-	DeleteOperationLog(ctx context.Context, logID int) error
-	ClearOperationLogs(ctx context.Context) error
-
-	// 登录日志
-	CreateLoginLog(ctx context.Context, log *dto.LoginLogCreateRequest) error
-	ListLoginLogs(ctx context.Context, query dto.LogQueryRequest) (*dto.PageResponse, error)
-	DeleteLoginLog(ctx context.Context, logID int) error
-	ClearLoginLogs(ctx context.Context) error
+	CreateOperationLog(ctx *gin.Context, log *dto.OperationLogCreateDTO) error
+	ListOperationLogs(ctx *gin.Context, page, pageSize int) (*dto.OperationLogInfo, error)
+	DeleteOperationLog(ctx *gin.Context, logID int) error
+	ClearOperationLogs(ctx *gin.Context) error
 }
